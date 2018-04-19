e98f26d20ffaf1e4c545c63bc72635413994cea9
haniokasai <htek@haniokasai.com>
2018-04-19 22:33:59 +0900

    test code

--- src/haniokasai/FlowExample.php
+++ src/haniokasai/FlowExample.php
@@ -1,5 +1,18 @@
 namespace FlowyExample;
 
-use Flowy\Flowy;
+use Flowy\Flowy\Flowy;
 use function Flowy\listen;
 
+use pocketmine\event\player\PlayerMoveEvent;
+
+class HeySiri extends Flowy{
+	function onEnable(){
+		$this->start($this->test());
+	}
+
+　　	function test(){
+		$event = yield listen(PlayerJoinEvent::class);
+		$player = $event->getPlayer();
+		$player->sendMessage(“moving”);
+	}
+}
